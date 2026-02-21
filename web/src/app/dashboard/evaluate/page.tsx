f036155dfb94361f8fff2a6c3ab76808f4dab1c0
Raul <corsal2025@users.noreply.github.com>
2026-02-20 23:56:05 -0300

    fix: reorder use client directive to be first line

--- web/src/app/dashboard/evaluate/page.tsx
+++ web/src/app/dashboard/evaluate/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic'
-
 "use client";
+
+export const dynamic = 'force-dynamic'
 
 import { useEffect, useState } from "react";
 import { Search, UserCircle2, ChevronRight, Activity } from "lucide-react";
